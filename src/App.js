b071411a8729fd9f02547f691051d982b172b572
Grigiriy <grigiriy.malyshev@gmail.com>
2020-02-09 13:44:17 +0300

    productCards base

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <MainScreen />
-      <div className="container-fluid d-flex">
+      <div className="container fluid d-flex no-wrap">
         <PromoCard
           center={false}
           image={'left'}
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
+import card_a from '../../assets/images/loveBox.jpg';
+import card_b from '../../assets/images/coupleBox.jpg';
+import card_c from '../../assets/images/heart.png';
+import card_d from '../../assets/images/loveBox.jpg';
+import card_e from '../../assets/images/coupleBox.jpg';
+import card_f from '../../assets/images/heart.png';
+
+import './styles.scss';
+
 class ProductCard extends Component {
   render() {
     return (
-      <div>
-        <div className="attributes">
-          <p>Название</p>
-          <p>Цена</p>
+      <div className="card">
+        <div className="card_image">
+          <img src={card_a} alt="" />
+        </div>
+        <div className="card_attributes">
+          <p>Drums</p>
+          <p>1600 ₽</p>
         </div>
       </div>
     );
