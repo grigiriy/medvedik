1face90322af915573a5ab9c7a96991f8789e5d9
Grigiriy <grigiriy.malyshev@gmail.com>
2020-02-11 01:15:17 +0300

    start extracting content to separete files

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 
-import card_a from '../../assets/images/loveBox.jpg';
-import card_b from '../../assets/images/coupleBox.jpg';
-import card_c from '../../assets/images/heart.png';
-import card_d from '../../assets/images/loveBox.jpg';
-import card_e from '../../assets/images/coupleBox.jpg';
-import card_f from '../../assets/images/heart.png';
-
 import './styles.scss';
-
 class ProductCard extends Component {
   render() {
+    const { name, price, imageSrc } = this.props.content;
     return (
       <div className="card">
         <div className="card_image">
-          <img src={card_a} alt="" />
+          <img src={require('../../assets/images/' + imageSrc)} alt="" />
         </div>
         <div className="card_attributes">
-          <p>Drums</p>
-          <p>1600 ₽</p>
+          <p>{name}</p>
+          <p>{price}</p>
         </div>
       </div>
     );
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 
 import ProductCard from '../ProductCard';
 
+import ProductsList from '../../assets/db/productList.js';
+
 class ProductList extends Component {
   render() {
     return (
       <div class="container">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {ProductsList.products.map((item, index) => (
+          <ProductCard content={item} />
+        ))}
       </div>
     );
   }
