39b302c9c8f5dffd66fe48c58a7d751ccdba3799
Grigiriy <grigiriy.malyshev@gmail.com>
2020-02-28 11:08:00 +0300

    refactor

--- src/components/PreviewCard/PreviewCard.jsx
+++ src/components/PreviewCard/PreviewCard.jsx
@@ -3,55 +3,27 @@ import React, { Component } from 'react';
 import './styles.scss';
 
 class PreviewCard extends Component {
-  state = {
-    atts: false,
-    lb: {
-      content: {}
-    }
-  };
-
-  toggleDescription = async e => {
-    this.setState({
-      atts: e
-    });
-  };
-
-  setContent = async (imageSrc, description, name) => {
-    this.setState({
-      lb: {
-        content: {
-          img: imageSrc,
-          text: description,
-          name: name
-        }
-      }
-    });
-    setTimeout(() => {
-      this.props.initLightBox(!this.props.lightBox, this.state.lb.content);
-    }, 0);
+  setContent = (imageSrc, description, name) => {
+    let content = {
+      img: imageSrc,
+      text: description,
+      name: name
+    };
+    this.props.initLightBox(!this.props.lightBox, content);
   };
 
   render() {
     const { name, description, imageSrc } = this.props.content;
-    const { atts } = this.state;
-
-    let classList = 'detail';
-
-    if (atts) {
-      classList += ' withAtts';
-    }
 
     return (
       <div
         className="preview"
-        onMouseEnter={() => this.toggleDescription(true)}
-        onMouseLeave={() => this.toggleDescription(false)}
         onClick={() => {
           this.setContent(imageSrc, description, name);
         }}
       >
         <img src={require('../../assets/images/' + imageSrc)} alt="" />
-        <div className={classList}>
+        <div className="detail">
           <p>{name}</p>
           <p>{description}</p>
         </div>
