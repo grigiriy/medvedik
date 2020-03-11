0087a1e23c1c1ebf9213bd6a4df1db995ac53199
Grigiriy <grigiriy.malyshev@gmail.com>
2020-03-11 06:36:55 +0300

    contacts in burger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { Link } from 'gatsby';
 import './styles.scss';
 
 import Content from '../../assets/db/navMenu.js';
+import Contacts from '../../assets/db/about.js';
 
 class Header extends Component {
   state = {
@@ -17,8 +18,9 @@ class Header extends Component {
   };
 
   render() {
+    let { credits } = Contacts;
     return (
-      <header className={this.state.open && 'active'}>
+      <header className={this.state.open ? 'active' : ''}>
         <ul className="topMenu">
           {Content.items.map(item => (
             <li key={item.link}>
@@ -31,9 +33,27 @@ class Header extends Component {
           ))}
           <li key="ig" className="igSvg">
             <a href="https://www.instagram.com/mashamedvedik/">
-              <img src={require('../../images/ig.svg')} alt="" />
+              {this.props.mobile ? (
+                <span className="credits">@mashamedvedik</span>
+              ) : (
+                <img src={require('../../images/ig.svg')} alt="" />
+              )}
             </a>
           </li>
+
+          {this.props.mobile && (
+            <>
+              <hr />
+              <li key="phone" className="credits">
+                <a href={`tel:${credits.phone.replace(/[\-, ]/g, '')}`}>
+                  {credits.phone}
+                </a>
+              </li>
+              <li key="email" className="credits">
+                <a href={`mailto:${credits.email}`}>{credits.email}</a>
+              </li>
+            </>
+          )}
         </ul>
         {this.props.mobile && (
           <div
