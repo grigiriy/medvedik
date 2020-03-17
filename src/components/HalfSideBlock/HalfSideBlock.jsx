81fa7b97b1355681e8ff4d6fd7acf0ff3773ce46
Grigiriy <grigiriy.malyshev@gmail.com>
2020-03-17 05:15:19 +0300

    mend
    new button type

--- src/components/HalfSideBlock/HalfSideBlock.jsx
+++ src/components/HalfSideBlock/HalfSideBlock.jsx
@@ -1,118 +1,73 @@
 import React, { Component } from 'react';
-import { Link } from 'gatsby';
+
+import TextBlock from './TextBlock.jsx';
+import ProductBlock from './ProductBlock.jsx';
+import ImagedBlock from './ImagedBlock.jsx';
+import OrderBlock from './Order/OrderBlock.jsx';
+// import ConstructorBlock from './ConstructorBlock.jsx';
 
 import './styles.scss';
 
 class HalfSideBlock extends Component {
   state = {
     color: null,
-    size: null
+    size: null,
+    type: 'shirt'
   };
 
   selectAtt = (value, att) => {
-    console.log(att);
     this.setState({
       [att]: value
     });
   };
 
+  setColor = e => {
+    this.setState({
+      color: e
+    });
+  };
+
   render() {
-    const { imageSrc, headLine, text, price, sizes, colors } = this.props.side;
-
-    const { mobile } = this.props.mobile;
-    const { classNames } = this.props;
-
-    function ImagedBlock(mobile) {
-      let classList = mobile ? 'halfScreenBlock mobileView' : 'halfScreenBlock';
-      classList = classNames ? classList + classNames : classList;
-      return (
-        <div
-          className={classList}
-          style={{
-            backgroundImage: `url(${require('../../images/' + imageSrc)})`
-          }}
-        >
-          <p className="h3">{headLine}</p>
-        </div>
-      );
-    }
-
-    function TextBlock(mobile) {
-      let classList = mobile
-        ? 'halfScreenBlock textOnly mobileView'
-        : 'halfScreenBlock textOnly';
-      return (
-        <div className={classList}>
-          <p className="h4">{headLine}</p>
-          <p>{text}</p>
-        </div>
-      );
-    }
-
-    function ProductBlock(props) {
-      let classList = mobile
-        ? 'halfScreenBlock textOnly mobileView'
-        : 'halfScreenBlock textOnly';
-      return (
-        <div className={classList}>
-          <p className="h4 header">{headLine}</p>
-          <p>{text}</p>
-          <p className="attributes">
-            <span>Ручная вышивка</span>
-            <span>100% хлопок</span>
-            <span className="grouped">
-              {sizes.map((item, index) => (
-                <span
-                  key={item}
-                  onClick={() => {
-                    props.selectAtt(item, 'size');
-                  }}
-                >
-                  {item}
-                </span>
-              ))}
-            </span>
-            <span className="small">Размерная сетка</span>
-            <span className="grouped">
-              {colors.map((item, index) => (
-                <span
-                  key={item[1]}
-                  onClick={() => {
-                    props.selectAtt(item[1], 'color');
-                  }}
-                  className="colorItem"
-                  style={{
-                    backgroundColor: item[1]
-                  }}
-                ></span>
-              ))}
-            </span>
-          </p>
-
-          <div className="ordering">
-            <p className="h4">{price} ₽</p>
-            <Link to={'/'}>
-              <button>Заказать</button>
-            </Link>
-          </div>
-
-          <div className="orderDirections">
-            <p>
-              Обратите внимание, что отправка производится через 2-3 дня после
-              заказа.
-            </p>
-            <Link to={'/'}>Подробнее</Link>
-          </div>
-        </div>
-      );
-    }
+    const { imageSrc, price, order } = this.props.side;
 
     if (imageSrc) {
-      return <ImagedBlock />;
+      return (
+        <ImagedBlock
+          side={this.props.side}
+          classNames={this.props.classNames}
+          mobile={this.props.mobile}
+        />
+      );
     } else if (price) {
-      return <ProductBlock selectAtt={this.selectAtt} />;
+      if (order) {
+        return (
+          <OrderBlock
+            side={this.props.side}
+            classNames={this.props.classNames}
+            mobile={this.props.mobile}
+            setColor={this.setColor}
+            selectAtt={this.selectAtt}
+            type={this.state.type}
+            size={this.state.size}
+            color={this.state.color}
+          />
+        );
+      }
+      return (
+        <ProductBlock
+          selectAtt={this.selectAtt}
+          side={this.props.side}
+          mobile={this.props.mobile}
+        />
+      );
     }
-    return <TextBlock />;
+    return (
+      <TextBlock
+        side={this.props.side}
+        classNames={this.props.classNames}
+        mobile={this.props.mobile}
+      />
+    );
   }
 }
 
