5e1edfd00f591714b914e7c8b99a5c3d10eb79c4
Grigiriy <grigiriy.malyshev@gmail.com>
2020-05-03 23:41:30 +0300

    head

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import { Helmet } from 'react-helmet';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
@@ -10,6 +11,59 @@ class Layout extends Component {
     let { children } = this.props;
     return (
       <>
+        <Helmet
+          // defer={true}
+          title="Maria Medvedik. Embroidery Artis"
+          link={[
+            {
+              rel: 'shortcut icon',
+              type: 'image/png',
+              href: `${require('../../images/fav.png')}`,
+            },
+          ]}
+          meta={[
+            {
+              name: 'description',
+              content: 'Маша Медведик - художница из москвы.',
+            },
+            {
+              name: 'keywords',
+              content: 'Embroidery Artis',
+            },
+            {
+              property: 'og:title',
+              content: 'Maria Medvedik. Embroidery Artis',
+            },
+            {
+              property: 'og:description',
+              content: 'Маша Медведик - художница из москвы.',
+            },
+            // {
+            //   property: 'og:image',
+            //   content: `${require('../../images/og.jpg')}`,
+            // },
+            // {
+            //   property: 'og:image:url',
+            //   content: `${require('../../images/og.jpg')}`,
+            // },
+            // {
+            //   property: 'og:image:width',
+            //   content: 1200,
+            // },
+            // {
+            //   property: 'og:image:height',
+            //   content: 630,
+            // },
+            // {
+            //   name: 'yandex-verification',
+            //   content: '74a94675657d77b9',
+            // },
+            // {
+            //   name: 'google-site-verification',
+            //   content: 'wGEqjkW29uPT70Dj8s9HwfjozrI884NdYdEYvlLKdxc',
+            // },
+          ]}
+        />
         <Header mobile={this.props.mobile} />
         {children}
         <Footer />
